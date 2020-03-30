1f1eb7e993d85f2761b4ea74b4f7e2c3bdd0927b
SimonLembeye <simon.lembeye@live.com>
2020-03-30 21:37:45 +0200

    fix: deploy test

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from project.settings import DEBUG
+from project.settings import DEBUG, MODE
 
 
 def index(request):
     return render(request, 'frontend/index.html', {
-        'debug': DEBUG
+        'debug': DEBUG,
+        'mode': MODE
     })
